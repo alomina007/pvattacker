7f3528b93a55d90041c9fe0f2aa93abf988b5ca2
alomina007 <alomina007@users.noreply.github.com>
2016-07-14 10:19:20 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 return {
-	bot_api_key = 'توکن شما',
-	admin = 207418094,186514905,131020409,
+	bot_api_key = '247921605:AAFPe0wbfEfqvTKbQuSd4aIDDju5akJTRNw',
+	admin = 208094932,
 	channel = '@Sudo_Tm', --channel username with the '@'
 
 	plugins = {
